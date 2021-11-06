6b88210c3850a5c7f7a5b3785ff92ec0fd37d215
Victor Dolgov <dolgov.vic95@gmail.com>
2021-11-06 19:41:05 +0700

    added ProfileViewController.swift

---
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Admin on 06.11.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
