d78558efaff84fb67ed64bba8b0ef331f5be0108
Victor Dolgov <dolgov.vic95@gmail.com>
2021-11-06 19:19:03 +0700

    added ProfileView

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Admin on 06.11.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
