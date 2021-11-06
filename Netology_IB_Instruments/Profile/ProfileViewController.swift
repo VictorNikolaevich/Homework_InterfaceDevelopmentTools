80315caa4415fd5e495360f76d639c2140e507ab
Victor Dolgov <dolgov.vic95@gmail.com>
2021-11-06 20:01:18 +0700

    Working project

--- Netology_IB_Instruments/Profile/ProfileViewController.swift
+++ Netology_IB_Instruments/Profile/ProfileViewController.swift
@@ -8,14 +8,16 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        let profileViewName = String(describing: ProfileView.self)
+        if let profileView = Bundle.main.loadNibNamed(profileViewName, owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 0, y: 20, width: view.bounds.width, height: 350)
+            view.addSubview(profileView)
+            profileView.customLabelName.text = "Имя"
+            profileView.customLabelCity.text = "Город"
+            profileView.customLabelDate.text = "Дата рождения"
+        }
     }
-    
-
-   
-
 }
