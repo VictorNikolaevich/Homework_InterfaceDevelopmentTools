aa94ccff00e7cc33081b5bbffa19caa251efb289
Victor Dolgov <dolgov.vic95@gmail.com>
2021-11-06 19:34:16 +0700

    added @IBOoutlet

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class ProfileView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var customLabelName: UILabel!
+    
+    @IBOutlet weak var customLabelDate: UILabel!
+    
+    @IBOutlet weak var customLabelCity: UILabel!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
 }
